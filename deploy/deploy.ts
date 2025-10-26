478062531ccf9818f07f0993de63c2c187de6feb
emilythorncode <cdmtzshca6174@outlook.com>
2025-10-26 15:03:54 +0800

    update deploy

--- deploy/deploy.ts
+++ deploy/deploy.ts
@@ -5,13 +5,51 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   const { deployer } = await hre.getNamedAccounts();
   const { deploy } = hre.deployments;
 
-  const deployedFHECounter = await deploy("FHECounter", {
+  const zamaToken = await deploy("ZamaToken", {
     from: deployer,
     log: true,
+    args: [],
   });
 
-  console.log(`FHECounter contract: `, deployedFHECounter.address);
+  const usdcToken = await deploy("USDCTestToken", {
+    from: deployer,
+    log: true,
+    args: [],
+  });
+
+  const ethToken = await deploy("ETHTestToken", {
+    from: deployer,
+    log: true,
+    args: [],
+  });
+
+  const confidentialZama = await deploy("ConfidentialZamaToken", {
+    from: deployer,
+    log: true,
+    args: [zamaToken.address],
+  });
+
+  const confidentialUSDC = await deploy("ConfidentialUSDCToken", {
+    from: deployer,
+    log: true,
+    args: [usdcToken.address],
+  });
+
+  const confidentialETH = await deploy("ConfidentialETHToken", {
+    from: deployer,
+    log: true,
+    args: [ethToken.address],
+  });
+
+  console.log("Deployed contracts:");
+  console.log(` - ZamaToken: ${zamaToken.address}`);
+  console.log(` - USDCTestToken: ${usdcToken.address}`);
+  console.log(` - ETHTestToken: ${ethToken.address}`);
+  console.log(` - ConfidentialZamaToken: ${confidentialZama.address}`);
+  console.log(` - ConfidentialUSDCToken: ${confidentialUSDC.address}`);
+  console.log(` - ConfidentialETHToken: ${confidentialETH.address}`);
 };
+
 export default func;
-func.id = "deploy_fheCounter"; // id required to prevent reexecution
-func.tags = ["FHECounter"];
+func.id = "deploy_zama_bridge_tokens";
+func.tags = ["ZamaBridge"];
